5f7831fad41ed16b9bd08a72f450d3841049e1b0
Namsakhi <namsakhikumar15@hotmail.com>
2022-03-23 13:57:35 -0700

    Revert "settings page first draft"
    This reverts commit 29e3c216a7655569313222b47aa14462ecb8ab52.

--- webserver/src/pages/brightness.js
+++
@@ -1,7 +0,0 @@
-angeInput = document.getElementById("range");
-container = document
-    .getElementsByClassName("container")[0];
-    
-rangeInput.addEventListener("change", function() {
-    container.style.filter = "brightness(" + rangeInput.value + "%)";
-});
