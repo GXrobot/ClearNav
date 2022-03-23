29e3c216a7655569313222b47aa14462ecb8ab52
Namsakhi <namsakhikumar15@hotmail.com>
2022-03-23 13:54:12 -0700

    settings page first draft

---
+++ webserver/src/pages/brightness.js
@@ -0,0 +1,7 @@
+angeInput = document.getElementById("range");
+container = document
+    .getElementsByClassName("container")[0];
+    
+rangeInput.addEventListener("change", function() {
+    container.style.filter = "brightness(" + rangeInput.value + "%)";
+});
