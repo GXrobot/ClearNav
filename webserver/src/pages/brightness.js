ef77b0b1a50782d034b5f400d395a81608e60b46
Namsakhi <namsakhikumar15@hotmail.com>
2022-03-25 07:00:27 -0700

    Updated settings pg - new files -linked index.html

--- webserver/src/pages/brightness.js
+++
@@ -1,7 +0,0 @@
-rangeInput = document.getElementById("range");
-container = document
-    .getElementsByClassName("container")[0];
-    
-rangeInput.addEventListener("change", function() {
-    container.style.filter = "brightness(" + rangeInput.value + "%)";
-});
