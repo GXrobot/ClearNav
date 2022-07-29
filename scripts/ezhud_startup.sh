4f703484d2edd47918b90298a8110e7ff36c90be
William Huong <willh.1298@gmail.com>
2022-07-29 02:26:50 -0700

    Needed to bump up the wait time for GPS UI

--- scripts/ezhud_startup.sh
+++ scripts/ezhud_startup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
+# Both the rotate and our GPS UI need some time before they work correctly
+sleep 3
+
 # For the 4" screen
 scrn=$(xrandr | grep -c 'HDMI-1 connected primary 480x800+0+0')
 if [ "$scrn" -eq "1" ]; then
-	# Rotating the screen needs a slightly longer wait, otherwise UI may grab vertical sizes
-	sleep 3
 	xrandr --output HDMI-1 --rotate left
-else
-	# Need a wait to avoid menubar issues
-	# 1 second only lets us see a blurred desktop background before UI loads
-	sleep 1
 fi
 
 # OpenDash's own service is broken, start ourselves
