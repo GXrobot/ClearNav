def1d867aab63e6c88022d54fdea220fa4e560b8
William Huong <willh.1298@gmail.com>
2022-07-30 14:12:12 -0700

    Removing get and set wifi mode, no longer applicable

--- webserver/src/EZhudSettings/EZhudSettings.js
+++ webserver/src/EZhudSettings/EZhudSettings.js
@@ -11,7 +11,6 @@ function getEZhudSettings() {
 	console.log('getEZhudSettings() called');
 
 	var currentSettings = {
-		'wifi_mode': getWifiMode(),
 		'wifi_country': getWifiCountry(),
 		'wifi_ssid': getWifiSSID(),
 		'wifi_psk': getWifiPSK()
@@ -36,11 +35,6 @@ function setEZhudSettings(newSettings) {
 
 		// TODO: Put the functions into a dictionary to get rid of this switch case
 		switch(key) {
-			case 'wifi_mode':
-				console.log('setEZhudSettings(): wifi_mode');
-				//setWifiMode(value);
-				rebootNeeded = true;
-				break;
 			case 'wifi_country':
 				console.log('setEZhudSettings(): wifi_country');
 				//setWifiCountry(value);
@@ -77,85 +71,6 @@ function setEZhudSettings(newSettings) {
 	}
 	
 	return getEZhudSettings();
-
-}
-
-// TODO: Fix later
-function getWifiMode() {
-
-	console.log('	getWifiMode()');
-
-	return 'Client';
-
-	var bashCmd = `grep ${CS_ENV_FILE} -e ENABLE_HOTSPOT`;
-	var wifiMode = 'Error';
-
-	try {
-		var res = execSync(bashCmd);
-		console.log(`	getWifiMode(): res.toString()=${res.toString()}`);
-		let hotspotEnabled = (res.toString().split('='))[1];
-		console.log(`	getWifiMode(): hotspotEnabled=${hotspotEnabled}`);
-		wifiMode = (hotspotEnabled == 1) ? 'hotspot' : 'client';
-		console.log(`	getWifiMode(): wifiMode=${wifiMode}`);
-	} catch(err) {
-		console.log('	getWifiMode(): err', err);
-		console.log('	getWifiMode(): stderr', err.stderr.toString());
-	}
-
-	return wifiMode;
-
-}
-
-// TODO: Fix later
-function setWifiMode(mode) {
-
-	console.log('	setWifiMode()');
-
-	return 0;
-
-	if( mode != 'hotspot' && mode != 'client' ) {
-		console.log(`	setWifiMode(): Unexpected wifi mode: ${mode}`);
-		return 1;
-	}
-
-	// Wifi mode can be set through crankshaft_env.sh
-	if( mode == 'hotspot' ) {
-		// TODO: Should the previous client ssid and psk be saved?
-		try {
-			// Set hotspot mode
-			execSync(`sudo sed -i 's/ENABLE_HOTSPOT=0/ENABLE_HOTSPOT=1/' ${CS_ENV_FILE}`);
-			// Unset client ssid and psk
-			execSync(`sudo sed -i 's/WIFI_SSID=.*/WIFI_SSID="sample"/' ${CS_ENV_FILE}`);
-			execSync(`sudo sed -i 's/WIFI_PSK=.*/WIFI_PSK="sample"/' ${CS_ENV_FILE}`);
-			execSync(`sudo sed -i 's/WIFI2_SSID=.*/WIFI2_SSID="sample"/' ${CS_ENV_FILE}`);
-			execSync(`sudo sed -i 's/WIFI2_PSK=.*/WIFI2_PSK="sample"/' ${CS_ENV_FILE}`);
-			// Force recreate wpa_supplicant.conf update
-			execSync(`sudo sed -i 's/WIFI_UPDATE_CONFIG=0/WIFI_UPDATE_CONFIG=1/' ${CS_ENV_FILE}`);
-			return 0;
-		} catch(err) {
-			console.log('	setWifiMode(): Failed to set hotspot mode');
-			console.log('	setWifiMode(): err', err);
-			console.log('	setWifiMode(): stderr', err.stderr.toString());
-		}
-	} else if( mode == 'client' ) {
-		try {
-			// Disable hotspot
-			execSync(`sudo sed -i 's/ENABLE_HOTSPOT=1/ENABLE_HOTSPOT=0/' ${CS_ENV_FILE}`);
-			// At this point we cannot know for sure what the wifi credentials will be
-			// Leave them blank. Also means no point in forcing a wpa_supplicant.conf refresh
-			// res = execSync(`sudo sed -i 's/WIFI_UPDATE_CONFIG=*/WIFI_UPDATE_CONFIG=1/' ${CS_ENV_FILE}`);
-			return 0;
-		} catch(err) {
-			console.log('	setWifiMode(): Failed to set client mode');
-			console.log('	setWifiMode(): err', err);
-			console.log('	setWifiMode(): stderr', err.stderr.toString());
-		}
-	} else {
-		console.log(`	setWifiMode(): Unexpected wifi mode: ${mode}`);
-		return 1;
-	}
-
-	return 1;
 
 }
 
