cc6252b35ddc780bc0e5d9f091f362c7a7b75bf1
Spenny1068 <31759569+Spenny1068@users.noreply.github.com>
2022-07-31 14:31:24 -0700

    getsinglevideo working

--- webserver/src/pages/dashcam.js
+++ webserver/src/pages/dashcam.js
@@ -2,7 +2,6 @@ var baseUrl = "localhost";
 // var baseUrl = "http://169.254.1.2";
 
 // This function queries the server for updated settings
-// Expects the settings and their values to be returned as a JSON
 function getUpdatedVideoFiles() {
     console.log('Called getUpdatedVideoFiles()');
 
@@ -18,17 +17,17 @@ function getUpdatedVideoFiles() {
 	xhttp.onreadystatechange = function() {
 		// Check that the response status code indicates success
 		if( this.readyState == 4 && this.status == 200 ) {
-			// console.log("response received:" + this.responseText);
-
             // update the "Videos" page with the new videos
             updatePage(this.responseText);
 		} 
     };
+
 	// Send the request
 	console.log("Sending GET update");
 	xhttp.send();
 }
 
+// this function fetches a specific video file from server.
 function getVideoFile(file) {
     console.log("Called getVideoFile(): " + file);
 
@@ -36,15 +35,17 @@ function getVideoFile(file) {
 	var xhttp = new XMLHttpRequest();
 
 	// Set up the request
-	xhttp.open('GET', baseUrl + '/recordings/videoList', true);
+	xhttp.open('GET', '/recordings/' + file, true);
 	xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
 	// Define the callback for when we get a response
 	xhttp.onreadystatechange = function() {
 		// Check that the response status code indicates success
 		if( this.readyState == 4 && this.status == 200 ) {
-			// Do something useful with the data...
-			// console.log(this.responseText);
+			// console.log("response: " + this.responseText);
+            
+            // open url in new tab
+            window.open('/recordings/' + file, '_blank').focus();
         } 
     };
 	// Send the request
@@ -80,8 +81,6 @@ function fixshit(fl) {
 // updates page with new list of videos
 function updatePage(filelist) {
 
-    // filelist = ["2022-08-01_23-24-12.mp4", "2022-08-02_23-24-12.mp4", "2022-08-03_23-24-12.mp4", "2022-08-01_23-14-12.mp4"];
-    // filelist = ["2022-08-01_23-24-12.mp4"];
     container_count = 1;
     daylist = [];
 
@@ -93,7 +92,6 @@ function updatePage(filelist) {
     cards.forEach(card => {
         card.remove();
     });
-    
 
     // populate page with videos
     for (var day = 31; day >= 1; day--) {         // loop over days
@@ -120,11 +118,6 @@ function updatePage(filelist) {
 
             // add buttons to new video-container div
             for (var x = 1; x < arr.length + 1; x++) {
-
-                // var newDiv = document.createElement('div');
-                // newDiv.classList.add('card');
-                // newDiv.appendChild(document.createTextNode(x))
-                // document.getElementById(container_div).appendChild(newDiv);
 
                 var newButton = document.createElement('input');
                 newButton.type = "button";
