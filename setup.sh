ba8739e044eed094b29af1fc0c5152be1c4e90bd
William Huong <willh.1298@gmail.com>
2022-07-31 18:31:47 -0700

    Initial changes to setup script, untested

--- setup.sh
+++ setup.sh
@@ -70,52 +70,49 @@ cd /home/pi
 git clone https://github.com/openDsh/dash.git
 
 cd dash
+
+# Skip starting OpenDash when it is built
+# It blocks the rest of the script until stopped
+sed -i 's/cd ..\/bin/#cd ../bin/' ./install.sh
+sed -i 's/.\/dash/#.\/dash/' ./install.sh
+
 ./install.sh
 
 # Change some settings
 # All of these settings could be done through raspi-config
 
+# Set menubar to autohide
+echo "Hiding menubar"
+sudo sed -i 's/autohide=./autohide=1/' /home/pi/.config/lxpanel/LXDE-pi/panels/panel
+sudo sed -i 's/heightwhenhidden=./heightwhenhidden=1/' /home/pi/.config/lxpanel/LXDE-pi/panels/panel
+
+# Set boot splash
+echo "Setting boot splash"
+sudo cp /usr/share/plymouth/themes/pix/splash.png /usr/share/plymouth/themes/pix/splash.png.background
+sudo cp /home/pi/ClearNav/webserver/src/assets/logo.png /usr/share/plymouth/themes/pix/splash.png
+
+# Set desktop background, hide trash bin
+echo "Setting desktop background"
+sudo sed -i 's@wallpaper=.*@wallpaper=/usr/share/plymouth/themes/pix/splash.png@' /etc/xdg/pcmanfm/LXDE-pi/desktop-items-0.conf
+sudo sed -i 's/wallpaper_mode=.*/wallpaper_mode=fit/' /etc/xdg/pcmanfm/LXDE-pi/desktop-items-0.conf
+
+echo "Hiding trash bin"
+sudo sed -i 's/show_trash=.*/show_trash=0/' /etc/xdg/pcmanfm/LXDE-pi/desktop-items-0.conf
+
 # Enable legacy camera
-echo "Enabling camera"
-
-sudo sed -i 's/^#\?camera_auto_detect=[01]//' /boot/config.txt
-
-if grep -qE "^#?start_x=[01]" /boot/config.txt; then
-	sudo sed -i 's/^#\?start_x=[01]/start_x=1/' /boot/config.txt
-else
-	sudo bash -c "echo 'start_x=1' >> /boot/config.txt"
-fi
-
-if grep -qE "^#?gpu_mem=[1234567890]*" /boot/config.txt; then
-	sudo sed -i 's/^#\?gpu_mem=[1234567890]*/gpu_mem=128/' /boot/config.txt
-else
-	sudo bash -c "echo 'gpu_mem=128' >> /boot/config.txt"
-fi
+echo "Enabling legacy camera"
+sudo raspi-config nonint do_legacy 0
 
 # The WaveShare CM4-NANO-B requires a custom device tree for camera support
 echo "Copying over device tree"
-
 sudo cp WS-dt-blob.bin /boot/dt-blob.bin
-
-# Every time we configure the Pi manually raspi-config switches from KMS to Fake KMS
-echo "Changing to fkms"
-
-if grep -qE "^#?dtoverlay=vc4-f?kms-v3d" /boot/config.txt; then
-	sudo sed -i 's/^#\?dtoverlay=vc4-f\?kms-v3d/dtoverlay=vc4-fkms-v3d/' /boot/config.txt
-else
-	sudo bash -c "echo 'dtoverlay=vc4-fkms-v3d' >> /boot/config.txt"
-fi
 
 # Enable UART
 echo "Enabling UART"
-
-if grep -qE "^#?enable_uart=[01]" /boot/config.txt; then
-	sudo sed -i 's/^#\?enable_uart=[01]/enable_uart=1/' /boot/config.txt
-else
-	sudo bash -c "echo 'enable_uart=1' >> /boot/config.txt"
-fi
+sudo raspi-config nonint do_serial 2
 
 # Set HDMI boost to 7
+# Not able to find raspi-config switch for this
 echo "Setting HDMI boost"
 
 if grep -qE "^#?config_hdmi_boost=[01234567]" /boot/config.txt; then
@@ -127,21 +124,10 @@ fi
 # Disable screen blanking
 # This is copied from raspi-config
 echo "Disabling screen blanking"
-
-sudo rm -f /etc/X11/xorg.conf.d/10-blanking.conf
-sudo sed -i '/^\o033/d' /etc/issue
-
-sudo mkdir /etc/X11/xorg.conf.d/
-sudo cp /usr/share/raspi-config/10-blanking.conf /etc/X11/xorg.conf.d/
-sudo bash -c "printf '\\033[9:0]' >> /etc/issue"
-
-# Set menubar to autohide
-echo "Hiding menubar"
-
-sudo sed -i 's/autohide=./autohide=1/' /home/pi/.config/lxpanel/LXDE-pi/panels/panel
-sudo sed -i 's/heightwhenhidden=./heightwhenhidden=1/' /home/pi/.config/lxpanel/LXDE-pi/panels/panel
+sudo raspi-config nonint do_blanking 1
 
 # Enable the USB port on the CM4
+# Not able to find raspi-config switch for this
 echo "Enabling CM4 USB"
 
 sudo sed -i 's/^#\?otg_mode=[01]//' /boot/config.txt
@@ -154,9 +140,7 @@ fi
 
 # Change hostname
 echo "Changing hostname"
-
-sudo sed -i 's/.*/EZhud/' /etc/hostname
-sudo sed -i 's/127.0.1.1.*/127.0.1.1\tEZhud/g' /etc/hosts
+sudo raspi-config nonint do_hostname 'EZhud'
 
 sync
 
@@ -165,4 +149,3 @@ sleep 5
 sudo reboot
 
 exit 0
-
